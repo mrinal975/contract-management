264001dc5e4ff8a6514de9faa0a6977f4cd7b497
mrinal975 <mrinalmallik1@gmail.com>
2023-02-16 14:27:43 +0600

    navbar and search bar created

--- src/views/ContactManager.vue
+++ src/views/ContactManager.vue
@@ -9,6 +9,24 @@
                     New
                 </router-link>
             </p>
+            <p class="fst-italic">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+            </p>
+            <form>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="row">
+                            <div class="col-md-8">
+                                <input type="text" class="form-control" placeholder="Search Name">
+                            </div>
+                            <div class="col-md-4">
+                                <input type="submit" class="btn btn-outline-dark">
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
 </template>
