0230f484467379f37da4cb711e2af0f81e83bb35
mrinal975 <mrinalmallik1@gmail.com>
2023-02-16 11:44:15 +0600

    package imported

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <h2>App component</h2>
+  <button class="btn btn-success">op</button>
+  <i class="fa fa-home text-danger fa-4x"></i>
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+// Font Awesome Icons
+import "../node_modules/@fortawesome/fontawesome-free/css/all.css";
+// Bootstrap Bundle CSS, JS
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+// Main css file
+import "./assets/style.css";  
+
 
 createApp(App).mount('#app')
